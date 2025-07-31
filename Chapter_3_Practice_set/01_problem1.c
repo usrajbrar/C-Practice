a24a5b739aff93adf4de1c71a8d0177db2e114cf
Usraj Singh Brar <techcodecsu@gmail.com>
2025-07-31 06:41:47 +0530

    Added Chapter_3_Practice Folder Replaced

---
+++ Chapter_3_Practice_set/01_problem1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    if(a = 11)// assignment operator (not equals to operator)
+    {
+        printf("I am 11:");
+    }
+    else
+    {
+        printf("I am 10");
+    }
+
+    return 0;
+}
---
+++ Chapter_3_Practice_set/04_problem4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int y;
+    printf("Enter the year: ");
+    scanf("%d", &y);
+    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+    {
+        printf("The year %d is a leap year\n", y);
+    }
+    else
+    {
+        printf("The year %d is not a leap year\n", y);
+    }
+    return 0;
+}
